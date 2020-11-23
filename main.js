25c1a5b56cf7dd552493f85bbb93ef565aa019e8
bit0101jp <try.jpn@gmail.com>
2020-11-23 16:54:08 +0900

    add particles

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 //  forked: https://codepen.io/onom/pen/JOwxZr
 
 let frame = 0;
-let starVertices = [];
+const starVertices = [];
 const width = window.innerWidth;
 const height = window.innerHeight;
 
-// star vertex coordinates
+// define the coordinates of the vertices of a star
 for (let degree = 18; degree < 360; degree += 36){
     starVertices.push(
-        new THREE.Vector3(Math.sin(degree /180 * Math.PI),
-                          Math.cos(degree /180 * Math.PI),
+        new THREE.Vector3(Math.sin(degree / 180 * Math.PI),
+                          Math.cos(degree / 180 * Math.PI),
                           0));
 }
 
-let starFaces = [
+// define the face of a star
+const starFaces = [
     new THREE.Face3( 0,  9,  1),
     new THREE.Face3( 2,  1,  3),
     new THREE.Face3( 4,  3,  5),
@@ -37,13 +38,16 @@ for (let i in starVertices){
     }
 }
 
-let webgldev = document.getElementById('webgl-dev');
-let renderer = new THREE.WebGLRenderer();
+// defined a renderer
+const webgldev = document.getElementById('webgl-dev');
+const renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
 webgldev.appendChild(renderer.domElement);
 
-let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(
+const scene = new THREE.Scene();
+
+// defined a camera
+const camera = new THREE.PerspectiveCamera(
     45,
     width / height,
     0.4,
@@ -53,70 +57,89 @@ camera.position.z = 190;
 camera.position.y = 50;
 camera.rotation.x = -0.0;
 
-let ambientLight = new THREE.AmbientLight( 0xffffff, 1);
+// defined a ambient light
+const ambientLight = new THREE.AmbientLight( 0xffffff, 1);
 scene.add( ambientLight );
 
-function createCircleCanvas() {
-    let canvas = document.createElement('canvas');
-    let SIZE   = 256;
-    let HALF   = SIZE / 2;
-    let CENTER = SIZE / 2;
-    canvas.width  = SIZE;
-    canvas.height = SIZE;
-    let context = canvas.getContext('2d');
+// create a gradient texture of illuminated points
+function getTexturePoint(col) {
+    const canvas = document.createElement('canvas');
+    const size   = 256;
+    const half   = size / 2;
+    const center = size / 2;
+    canvas.width  = size;
+    canvas.height = size;
+    const context = canvas.getContext('2d');
 
-    let color = new THREE.Color();
-    color.setHex(0x888888);
+    const color = new THREE.Color();
+    color.setHex(col);
 
-    let grad = context.createRadialGradient(CENTER, CENTER, 0, CENTER, CENTER, HALF);
+    const grad = context.createRadialGradient(center, center, 0, center, center, half);
     grad.addColorStop(0, color.getStyle());
     grad.addColorStop(0.99, color.getStyle());
     grad.addColorStop(1, '#000000');
     context.beginPath();
-    context.arc(CENTER, CENTER, HALF, 0, Math.PI * 2);
+    context.arc(center, center, half, 0, Math.PI * 2);
     context.fillStyle = grad;
     context.fill();
     context.closePath();
 
-    let texture = new THREE.Texture(canvas);
+    const texture = new THREE.Texture(canvas);
     texture.needsUpdate = true;
     return texture;
 }
 
 // define the color of the stars
-let starMaterial = new THREE.MeshPhongMaterial( {
+const starMaterial = new THREE.MeshPhongMaterial( {
   color: 0xffffaa
 } );
 
 // create apex star geometry
-let geom = new THREE.Geometry();
+const geom = new THREE.Geometry();
 geom.vertices = starVertices;
 geom.faces = starFaces;
-geom.computeFaceNormals()
-let starObject = new THREE.Mesh(geom, starMaterial);
+geom.computeFaceNormals();
+const starObject = new THREE.Mesh(geom, starMaterial);
 
+// define the position of the star
 starObject.position.set( 0, 104, 0 );
 starObject.rotation.set( 0, 0, -60 );
 scene.add( starObject );
 
-let geometry = new THREE.Geometry();
+const geometry = new THREE.Geometry();
 for (let i = 0; i < 1000; i++) {
     geometry.vertices.push();
 }
 
-let material = new THREE.PointsMaterial({
-    map: createCircleCanvas(),
+const geometry2 = new THREE.Geometry();
+for (let i = 0; i < 1000; i++) {
+    geometry2.vertices.push();
+}
+
+const material = new THREE.PointsMaterial({
+    map: getTexturePoint(0x888888),
     size: 4,
     transparent: true,
     blending : THREE.AdditiveBlending
 });
 
-let mesh = new THREE.Points(geometry, material);
+const material2 = new THREE.PointsMaterial({
+    map: getTexturePoint(0xdddddd),
+    size: 2,
+    transparent: true,
+    blending : THREE.AdditiveBlending
+});
+
+const mesh = new THREE.Points(geometry, material);
 scene.add(mesh);
 
-function animation() {
-    let timing = frame % 241;
+const mesh2 = new THREE.Points(geometry2, material2);
+scene.add(mesh2);
 
+
+function animation() {
+    // define blinking illuminations
+    let timing = frame % 241;
     if (timing < 50){
         mesh.material.color.setHex(0x0000ff);
     }else if (timing < 100){
@@ -156,10 +179,17 @@ function animation() {
     mesh.material.colorsNeedUpdate = true;
 
     for (let i = 0; i < 1000; i++) {
-            let x = (Math.sin(i + frame / 100) * (50 - (i / 20)));
-            let y = i/10;
-            let z = (Math.cos(i + frame / 100) * (50 - (i / 20)));
-            geometry.vertices[ i ] = new THREE.Vector3(x, y, z);
+        let x = (Math.sin(i + frame / 100) * (40 - (i / 25)));
+        let y = i/10;
+        let z = (Math.cos(i + frame / 100) * (40 - (i / 25)));
+        geometry.vertices[ i ] = new THREE.Vector3(x, y, z);
+    }
+
+    for (let i = 0; i < 1000; i++) {
+        let x = (Math.sin(i + frame / 100) * (25 - (i / 40)));
+        let y = i/10;
+        let z = (Math.cos(i + frame / 100) * (25 - (i / 40)));
+        geometry2.vertices[ i ] = new THREE.Vector3(x, y, z);
     }
     geometry.verticesNeedUpdate = true;
 };
@@ -172,4 +202,3 @@ function render(){
 }
 
 render();
-
